3db0e014377b1f35213b88a5f7d61893999b0bb0
Maite Becker <beckermaite21@gmail.com>
2024-05-12 16:09:38 -0300

    Criando arquivo de tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        short num1 = 1;
+        int num2 = num1;
+        num1 = (short) num2; //preciso fazer o cast
+
+        final double PI = 3.14; //declaração de constante. Final, pois só posso atribuir o valor uma vez
+
+        //Declaração
+        String ola; //strings são objetos ou instâncias da classe String
+
+        //instânciação
+        ola = new String("Olá, mundo");
+
+        System.out.println(ola.length()); //método da classe String
     }
 }
