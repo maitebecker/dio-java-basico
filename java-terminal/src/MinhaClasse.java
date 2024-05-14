ca6c6f1caf5bf2c9128a7f1a31f25cf0115c43b3
Maite Becker <beckermaite21@gmail.com>
2024-05-14 20:58:04 -0300

    Criando as classes do projeto java-terminal

---
+++ java-terminal/src/MinhaClasse.java
@@ -0,0 +1,8 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Oi, fui executado pelo Terminal");
+    }
+}
+
+//cd C:\Users\Win10\dio-java\dio-java-basico\java-terminal\bin
+//java MinhaClasse
